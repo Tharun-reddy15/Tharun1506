565cd6eca94a1cb0e85ccd4adc684dc0eb76bf72
marri <marripatitharun@gmail.com>
2026-01-13 14:33:38 +0530

    initial commit

--- control structures.py
+++ control structures.py
@@ -14,3 +14,22 @@ else:
 
 for i in range(1,6):
     print(i)
+
+    j = 1
+    while j <= 5:
+        print(j)
+        j += 1
+        if j == 2:
+            break
+
+    day = 2
+    match day:
+        case 1:
+            print("Monday")
+        case 2:
+            print("Tuesday")
+
+        case 3:
+            print("Wednesday")
+        case 4:
+            print("Thrusday")
