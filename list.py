e72f48f2ae11a491cc79455671c6bf8bff8a8abf
marri <marripatitharun@gmail.com>
2026-01-13 11:51:14 +0530

    initial commit

--- list.py
+++ list.py
@@ -16,3 +16,20 @@ if 10 in numbers:
 
 matrix=[[1,2,3],[4,5,6]]
 print(matrix[1][2])
+
+matrix = [[1, 2, 3], [4, 5, 6]]
+print(matrix[1][2])
+
+names.reverse()
+print(names)
+names.append("kalai")
+print(names)
+
+names.extend(["pavan", "leela"])
+print(names)
+
+names.remove("kalai")
+
+print(names)
+
+names.insert(3, "uma")
