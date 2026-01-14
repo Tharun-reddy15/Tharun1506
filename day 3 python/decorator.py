126c9b401e04b3ae9f932562769a94a5fa545f50
marri <marripatitharun@gmail.com>
2026-01-14 15:53:25 +0530

    day3 practice.py

---
+++ day 3 python/decorator.py
@@ -0,0 +1,14 @@
+
+
+def mydecorator(func):
+    def wrapper():
+        print("Before Function Call")
+        func()
+        print("After Function Call")
+    return wrapper
+
+@mydecorator
+def sayhello():
+    print("Hello")
+
+sayhello()
