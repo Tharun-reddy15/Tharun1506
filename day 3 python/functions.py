71a965a00b0ef72851cac57568047d0bcbebeb7d
marri <marripatitharun@gmail.com>
2026-01-14 10:10:50 +0530

    day3 pratice

--- day 3 python/functions.py
+++ day 3 python/functions.py
@@ -6,3 +6,16 @@ def sub(a,b):
 
 add(10,20)
 print(sub(100,20))
+
+def hello(greeting='Hello', name='world'):
+    print('%s, %s!' % (greeting,name))
+hello('Greetings')
+
+def print_params(*params):
+    print(params)
+print_params('Testing')
+print_params(1,2,3)
+
+def print_params_3(**params):
+    print(params)
+print_params_3(x=1,y=2,z=3)
