92f06442fa0463430ef6832b644e78149fac74b6
marri <marripatitharun@gmail.com>
2026-01-16 10:43:29 +0530

    day 4

---
+++ day 4 python/class types.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class shape(ABC):
+    def display(self):
+        print("normal method")
+
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class rectangle(shape):
+    def area(self):
+        print("area method implemented")
+
+
+r = rectangle()
+r.area()
+r.display()
