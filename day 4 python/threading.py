722801a036f6e1dd5a0dbe85bf5ee619c7ac57ee
marri <marripatitharun@gmail.com>
2026-01-16 16:54:11 +0530

    lab

--- day 4 python/threading.py
+++ day 4 python/threading.py
@@ -7,3 +7,17 @@ def task():
     t.join()
 
 print("Main thread ends")
+
+import threading
+
+def task():
+    print("Thread is running")
+
+t1 = threading.Thread(target=task)
+t2 = threading.Thread(target=task, args=("Thread2",))
+t1.start()
+t2.start()
+t1.join()
+t2.join()
+
+print("Main thread ends")
