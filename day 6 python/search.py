38b2de2565a3cde0a33120905d798913950367a3
marri <marripatitharun@gmail.com>
2026-01-20 10:17:41 +0530

    project

--- day 6 python/search.py
+++ day 6 python/search.py
@@ -8,3 +8,14 @@ else:
 
 searchresult=re.search("python", text)
 print(searchresult.group())
+
+import re
+text="python is powerful"
+result=re.match("powerful", text)
+if result:
+    print("match found")
+else:
+    print("match not found")
+
+searchresult=re.search("is", text)
+print(searchresult.group())
